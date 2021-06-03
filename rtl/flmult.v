7b146f52bfda48ab7b81a7553d12c3d61662b1db
Hisham Elreedy <hishamelreedy1@gmail.com>
2021-06-04 00:52:12 +0200

    modifying float multiplier

--- rtl/flmult.v
+++ rtl/flmult.v
@@ -1,33 +1,62 @@
-`timescale 1ns / 1ps
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//File Name: Multiplication.v
+//Created By: Sheetal Swaroop Burada
+//Date: 30-04-2019
+//Project Name: Design of 32 Bit Floating Point ALU Based on Standard IEEE-754 in Verilog and its implementation on FPGA.
+//University: Dayalbagh Educational Institute
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 module flmult(
-	input [31:0] num1,
-	input [31:0] num2,
-	output [31:0] result
-    );
-    wire enable;
-    assign enable=1'b1;
-	reg [7:0] exp1,exp2;
-	reg [22:0] man1,man2;
-	wire [27:0] man_r;
-	reg [31:0] answer;
-	
-	//my paratmeterized modified booth encoded multiplier :p
-	Multiplier #(.N(14)) mul(.y({1'b1, man1[22:10]}), .x({1'b1, man2[22:10]}), .result(man_r) );
-	
-	always @ (*) begin
-		exp1 = num1[30:23];
-		exp2 = num2[30:23];
-		man1 = num1[22:0];
-		man2 = num2[22:0];
-		//exponent
-		answer[30:23] = (man_r[27])? (({1'b0,exp1} + {1'b0,exp2}) - 9'd126) :  (({1'b0,exp1} + {1'b0,exp2}) - 9'd127); // remove the bias
-		//mantissa
-		answer[22:0] = (man_r[27])? man_r[26:4] : man_r[25:3] ;
-		//sign
-		answer[31] = num1[31] ^ num2[31];
-	end
-	
-	assign result = (!num1||!num2)?32'h00000000: answer;
-	
+		input [31:0] a_operand,
+		input [31:0] b_operand,
+		//output Exception,Overflow,Underflow,
+		output [31:0] result
+		);
+
+wire sign,product_round,normalised,zero;
+wire [8:0] exponent,sum_exponent;
+wire [22:0] product_mantissa;
+wire [23:0] operand_a,operand_b;
+wire [47:0] product,product_normalised; //48 Bits
+
+
+assign sign = a_operand[31] ^ b_operand[31];
+
+//Exception flag sets 1 if either one of the exponent is 255.
+assign Exception = (&a_operand[30:23]) | (&b_operand[30:23]);
+
+//Assigining significand values according to Hidden Bit.
+//If exponent is equal to zero then hidden bit will be 0 for that respective significand else it will be 1
+
+assign operand_a = (|a_operand[30:23]) ? {1'b1,a_operand[22:0]} : {1'b0,a_operand[22:0]};
+
+assign operand_b = (|b_operand[30:23]) ? {1'b1,b_operand[22:0]} : {1'b0,b_operand[22:0]};
+
+assign product = operand_a * operand_b;			//Calculating Product
+
+assign product_round = |product_normalised[22:0];  //Ending 22 bits are OR'ed for rounding operation.
+
+assign normalised = product[47] ? 1'b1 : 1'b0;	
+
+assign product_normalised = normalised ? product : product << 1;	//Assigning Normalised value based on 48th bit
+
+//Final Manitssa.
+assign product_mantissa = product_normalised[46:24] + (product_normalised[23] & product_round); 
+
+assign zero = Exception ? 1'b0 : (product_mantissa == 23'd0) ? 1'b1 : 1'b0;
+
+assign sum_exponent = a_operand[30:23] + b_operand[30:23];
+
+assign exponent = sum_exponent - 8'd127 + normalised;
+
+assign Overflow = ((exponent[8] & !exponent[7]) & !zero) ; //If overall exponent is greater than 255 then Overflow condition.
+//Exception Case when exponent reaches its maximu value that is 384.
+
+//If sum of both exponents is less than 127 then Underflow condition.
+assign Underflow = ((exponent[8] & exponent[7]) & !zero) ? 1'b1 : 1'b0; 
+
+assign result = Exception ? 32'd0 : zero ? {sign,31'd0} : Overflow ? {sign,8'hFF,23'd0} : Underflow ? {sign,31'd0} : {sign,exponent[7:0],product_mantissa};
+
+
 endmodule
