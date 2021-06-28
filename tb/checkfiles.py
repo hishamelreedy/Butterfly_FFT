a05a1d6d16bf13047861320884938f70aa673f13
Hisham Elreedy <hishamelreedy1@gmail.com>
2021-06-28 16:38:21 +0200

    check simresults with truevalues

--- tb/checkfiles.py
+++ tb/checkfiles.py
@@ -1,9 +1,12 @@
-f = open("data/c2_output.txt")
-lines = f.readlines()
-lines = [each_string.upper() for each_string in lines]
-f = open("data/column2_output.txt")
-lines2 = f.readlines()
-b=set(lines2)
-new_list = [i for i, item in enumerate(lines) if item not in b]
-print(new_list)
-print(lines2[0]==lines[0])
+# Check Column 4 output
+#Sim Results
+f = open("data/c4_output.txt")
+simresults = f.readlines()
+simresults=simresults[3:]
+simresults = [each_string.upper() for each_string in simresults]
+#True Values
+f = open("data/column4_output.txt")
+truevalues = f.readlines()
+# Compare simresults with true values and return mismatch indices
+mismatch_indices = [i for i, item in enumerate(truevalues) if item not in simresults]
+print(mismatch_indices)
