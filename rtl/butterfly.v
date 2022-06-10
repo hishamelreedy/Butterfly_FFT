6a4eac2227631c7ca66477f55ead2da0c385945f
hishamelreedy <hishamelreedy@gmail.com>
2022-06-10 02:34:12 +0200

    fixed pipelining

--- rtl/butterfly.v
+++ rtl/butterfly.v
@@ -42,7 +42,7 @@ wire [64*32-1:0] outmacst0;
 fftc0 c0 (clk,reset,Reg_Input,outmacst0);
 //Save Output
 reg [64*32-1:0] Reg_outmacst0;
-always @(posedge clk)
+always @(negedge clk2)
  begin
     if (reset == 1)
      Reg_outmacst0 <= 2048'b0;
@@ -54,7 +54,7 @@ always @(posedge clk)
 wire [64*32-1:0] outmacst1;
 reg [64*32-1:0] Reg_outmacst1;
 fftc1 c1 (clk,reset,Reg_outmacst0,outmacst1);
-always @(posedge clk)
+always @(negedge clk2)
  begin
     if (reset == 1)
      Reg_outmacst1 <= 2048'b0;
@@ -68,7 +68,7 @@ always @(posedge clk)
 wire [64*32-1:0] outmacst2;
 reg [64*32-1:0] Reg_outmacst2;
 fftc2 c2 (clk,reset,Reg_outmacst1,outmacst2);
-always @(posedge clk)
+always @(negedge clk2)
  begin
     if (reset == 1)
      Reg_outmacst2 <= 2048'b0;
@@ -81,7 +81,7 @@ always @(posedge clk)
 wire [64*32-1:0] outmacst3;
 reg [64*32-1:0] Reg_outmacst3;
 fftc3 c3 (clk,reset,Reg_outmacst2,outmacst3);
-always @(posedge clk)
+always @(negedge clk2)
  begin
     if (reset == 1)
      Reg_outmacst3 <= 2048'b0;
@@ -95,7 +95,7 @@ always @(posedge clk)
 wire [64*32-1:0] outmacst4;
 reg [64*32-1:0] Reg_outmacst4,Reg_outmacst4_t,Reg_outmacst4_t2,Reg_outmacst4_t3,Reg_outmacst4_t4;
 fftc4 c4 (clk,reset,Reg_outmacst3,outmacst4);
-always @(posedge clk)
+always @(negedge clk2)
  begin
     if (reset == 1)
      Reg_outmacst4 <= 2048'b0;
--- run/runfft.do
+++ run/runfft.do
@@ -4,4 +4,7 @@ add wave -position insertpoint sim:/butterfly_tb/*
 add wave -position insertpoint sim:/butterfly_tb/FFT/Reg_Input
 add wave -position insertpoint sim:/butterfly_tb/FFT/c4/outmac
 add wave -position insertpoint sim:/butterfly_tb/FFT/clk2
+add wave -position insertpoint sim:/butterfly_tb/FFT/c0/*
+add wave -position insertpoint sim:/butterfly_tb/FFT/Reg_outmacst0
+add wave -position insertpoint sim:/butterfly_tb/FFT/outmacst0
 run -all
